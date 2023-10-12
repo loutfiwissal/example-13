e74c04a51caad0751dcb896d8113a5006095f469
Wissal Loutfi <147111691+loutfiwissal@users.noreply.github.com>
2023-10-12 02:40:24 +0100

    exercise-age

---
+++ exercise-age.py
@@ -0,0 +1,11 @@
+AGE=int(input("please type your age:" ))
+if AGE>=6 and AGE<=7:
+    print("poussin")
+elif AGE>=8 and AGE<=9:
+    print("pupille")
+elif AGE>=10 and AGE<=11:
+    print("minime")
+elif AGE>=12:
+    print("cadet")
+else:
+    print("age does not exist")
